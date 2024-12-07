f72df10c26c6238771000a80cc42a86358047595
keitaMax <86122925+NiheiKeita@users.noreply.github.com>
2024-12-07 10:20:13 +0900

    終始

--- src/rules/isEmpty.ts
+++ src/rules/isEmpty.ts
@@ -4,7 +4,7 @@ declare global {
   }
 }
 
-Array.prototype.isEmpty = function <T>(): boolean {
+Array.prototype.isEmpty = function (): boolean {
   return this.every(item => item === undefined) || this.length === 0;
 }
 
